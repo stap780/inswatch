bb1aca61f9e69c2adc0372ecf96a2ee02faf35df
stap780 <panaet80@mail.ru>
2025-12-22 13:32:37 +0300

    add update

--- app/controllers/mark_controller.rb
+++ app/controllers/mark_controller.rb
@@ -1,13 +1,12 @@
 class MarkController < ApplicationController
   before_action :require_authentication
 
-  # GET /mark/login
-  # Редирект на Mark с автологином
   def login
     return_url = params[:return_to] || params[:return_url]
     login_url = MarkService.mark_login_url(Current.user, return_url: return_url)
     
     redirect_to login_url, allow_other_host: true
   end
+  
 end
 
